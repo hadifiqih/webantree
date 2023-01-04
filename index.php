9e2c807f8796e0edfc3d6a40e1d2f043597b30ee
Hadi Fiqih <pratamahadi25@gmail.com>
2023-01-04 13:02:02 +0700

    tambah dokumentasi

--- index.php
+++ index.php
@@ -1,22 +1,24 @@
 <?php
+
 session_start();
 
 $divisi = $_SESSION['divisi'];
 
-    if(isset($_SESSION['login'])){
-        if($divisi == 'admwrk'){
-            header('location:admin-workshop.php');
-            exit;
-        }elseif($divisi == 'sales'){
-            header('location:sales.php');
-            exit;
-        }elseif($divisi == 'desopr'){
-            header('location:desopr.php');
-            exit;
-        }
-    }else{
-        header('location:login.php');
+if (isset($_SESSION['login'])) {
+    if ($divisi == 'admwrk') {
+        header('location:admin-workshop.php');
+        exit;
+    } elseif ($divisi == 'sales') {
+        header('location:sales.php');
+        exit;
+    } elseif ($divisi == 'desopr') {
+        header('location:desopr.php');
+        exit;
+    } elseif ($divisi == 'dokumentasi') {
+        header('location:div-dokumentasi.php');
         exit;
     }
-
-?>
+} else {
+    header('location:login.php');
+    exit;
+}
