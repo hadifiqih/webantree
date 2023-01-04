5b43ea88d7d99d9d0bcdda75254658a665b416ac
Hadi Fiqih <pratamahadi25@gmail.com>
2023-01-04 12:53:25 +0700

    update ambil nama kecil

--- proses-edit-file.php
+++ proses-edit-file.php
@@ -2,53 +2,50 @@
 
 session_start();
 
-if(!isset($_SESSION['login'])){
+if (!isset($_SESSION['login'])) {
     header('location:login.php');
     exit;
 }
 
 include('connection.php');
-if(isset($_POST['submitFile'])){
+if (isset($_POST['submitFile'])) {
+    $id = $_POST['antrian'];
+    $namaKecil = $_POST['namaKecil'];
 
-$id = $_POST['antrian'];
+    $namafile = $_FILES['uFile']['name'];
+    $ukuranfile = $_FILES['uFile']['size'];
 
-$namafile = $_FILES['uFile']['name'];
-$ukuranfile = $_FILES['uFile']['size'];
+    print_r($namafile);
 
-print_r($namafile);
-
-if($ukuranfile > 20971520){
+    if ($ukuranfile > 20971520) {
         header('location: list-antrian.php?pesan=size');
     }
-        if ($namafile != ""){
-            $izinEkstensi = array('cdr', 'pdf', 'ai');
-            $pisahEkstensi = explode('.',$namafile);
-            $ekstensi = strtolower(end($pisahEkstensi));
+    if ($namafile != "") {
+        $izinEkstensi = array('cdr', 'pdf', 'ai');
+        $pisahEkstensi = explode('.', $namafile);
+        $ekstensi = strtolower(end($pisahEkstensi));
 
-            $file_tmp = $_FILES['uFile']['tmp_name'];
-            $namaEnc = md5(rand(5,11));
+        $file_tmp = $_FILES['uFile']['tmp_name'];
+        $namaEnc = md5(random_int(5, 11));
 
-            $namaFileBaru = $namaEnc . '-' . $namafile;
+        $namaFileBaru = $namaEnc . '-' . $namafile;
 
-            if(in_array($ekstensi,$izinEkstensi) === true){
-                //----------------------------------------
-                move_uploaded_file($file_tmp, 'file/'.$namaFileBaru);
-                
-                $sql = "UPDATE data_antrian SET file_desain='$namaFileBaru' WHERE no_antrian='$id'";
-                $query = mysqli_query($connect, $sql);
+        if (in_array($ekstensi, $izinEkstensi) === true) {
+            //----------------------------------------
+            move_uploaded_file($file_tmp, 'file/'.$namaFileBaru);
 
-                if ($query) {
-                    header('location: list-antrian.php?pesan=fileberhasil');
-                } else {
-                    print_r(mysqli_error($connect));
-                }
+            $sql = "UPDATE data_antrian SET file_desain='$namaFileBaru', nama_desainer='$namaKecil' WHERE no_antrian='$id'";
+            $query = mysqli_query($connect, $sql);
+
+            if ($query) {
+                header('location: index.php');
+            } else {
+                print_r(mysqli_error($connect));
             }
-            else{
-                header('location: list-antrian.php?pesan=salahekstensi');
-            }
+        } else {
+            echo "Ekstensi Salah";
         }
-        else{
-            header('location: list-antrian.php?pesan=filekosong');
-        }
-
+    } else {
+        echo "File Kosong";
     }
+}
