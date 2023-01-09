eaa4bba4554dce72a5b977f45a3c2d3ab6ca3ca4
Hadi Fiqih <pratamahadi25@gmail.com>
2023-01-09 16:50:44 +0700

    hapus desainer

--- edit-antrian.php
+++ edit-antrian.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-if(!isset($_SESSION['login'])){
+if (!isset($_SESSION['login'])) {
     header('location:login.php');
     exit;
 }
@@ -53,139 +53,130 @@ if (mysqli_num_rows($query) < 1) {
 
 <body>
     <div class="container-fluid bg-warning py-5">
-    <div class="card mx-auto px-3" style="width: 60rem;">
+        <div class="card mx-auto px-3" style="width: 60rem;">
             <div class="card-body">
-        <h1 class="card-title">Edit Antrian Workshop</h1>
-        <br>
-        <form action="proses-edit.php" method="POST">
-            <div class="form-group">
-                <?php
+                <h1 class="card-title">Edit Antrian Workshop</h1>
+                <br>
+                <form action="proses-edit.php" method="POST">
+                    <div class="form-group">
+                        <?php
 
                 $noAntrianGabung = preg_replace("/[^a-zA-Z0-9]/", "", $dataEdit['tanggal_antrian']) . "-" . $dataEdit['no_antrian'];
 
-                echo "<label for='antrian'>Kode Antrian</label>";
-                echo "<input class='form-control' type='text' name='antrian' value=" . $noAntrianGabung . " readonly>";
-                echo "<input class='form-control' type='hidden' name='noAntrian' value=" . $dataEdit['no_antrian'] . ">";
-                echo "<input class='form-control' type='hidden' name='tanggal' value=" . $dataEdit['tanggal_antrian'] . ">";
+echo "<label for='antrian'>Kode Antrian</label>";
+echo "<input class='form-control' type='text' name='antrian' value=" . $noAntrianGabung . " readonly>";
+echo "<input class='form-control' type='hidden' name='noAntrian' value=" . $dataEdit['no_antrian'] . ">";
+echo "<input class='form-control' type='hidden' name='tanggal' value=" . $dataEdit['tanggal_antrian'] . ">";
 
-                echo "<br>";
-                ?>
+echo "<br>";
+?>
 
-                <label for="isSales">Sales</label>
-                <select class="form-control" name="isSales">
-                    <?php
-                    while ($resultSales = mysqli_fetch_array($querySales)) {
-                        if ($resultSales['salesName'] == $dataEdit['nama_sales']) {
-                            echo "<option value='" . $resultSales['salesName'] . "' selected>" . $resultSales['salesName'] . "</option>";
-                        } else {
-                            echo "<option value='" . $resultSales['salesName'] . "'>" . $resultSales['salesName'] . "</option>";
-                        }
-                    }
-                    ?>
-                </select>
-                <br>
+                        <label for="isSales">Sales</label>
+                        <select class="form-control" name="isSales">
+                            <?php
+    while ($resultSales = mysqli_fetch_array($querySales)) {
+        if ($resultSales['salesName'] == $dataEdit['nama_sales']) {
+            echo "<option value='" . $resultSales['salesName'] . "' selected>" . $resultSales['salesName'] . "</option>";
+        } else {
+            echo "<option value='" . $resultSales['salesName'] . "'>" . $resultSales['salesName'] . "</option>";
+        }
+    }
+?>
+                        </select>
+                        <br>
 
-                <label for="keywordStamp">Nama Stempel / Kata Kunci</label>
-                <input class="form-control" type="text" placeholder="Contoh : Stempel Es Madura" name="keywordStamp"
-                    value="<?php echo $dataEdit['keyword_stempel']; ?>">
-                <br>
+                        <label for="keywordStamp">Nama Stempel / Kata Kunci</label>
+                        <input class="form-control" type="text" placeholder="Contoh : Stempel Es Madura"
+                            name="keywordStamp" value="<?php echo $dataEdit['keyword_stempel']; ?>">
+                        <br>
 
-                <label for="whatJob">Jenis Pekerjaan</label>
-                <select class="form-control" id="whatJob" name="whatJob">
-                    <?php
-                    while ($resultProduk = mysqli_fetch_array($queryProduk)) {
-                        if ($resultProduk['nama_produk'] == $dataEdit['nama_pekerjaan']) {
-                            echo "<option selected>" . $resultProduk['nama_produk'] . "</option>";
-                        } else {
-                            echo "<option>" . $resultProduk['nama_produk'] . "</option>";
-                        }
-                    }
-                    ?>
-                </select>
-                <br>
+                        <label for="whatJob">Jenis Pekerjaan</label>
+                        <select class="form-control" id="whatJob" name="whatJob">
+                            <?php
+while ($resultProduk = mysqli_fetch_array($queryProduk)) {
+    if ($resultProduk['nama_produk'] == $dataEdit['nama_pekerjaan']) {
+        echo "<option selected>" . $resultProduk['nama_produk'] . "</option>";
+    } else {
+        echo "<option>" . $resultProduk['nama_produk'] . "</option>";
+    }
+}
+?>
+                        </select>
+                        <br>
 
-                <label for="startJob">Mulai</label>
-                <input class="form-control" type="datetime-local" name="startJob" id="startJob" value="<?= $dataEdit['mulai_kerja']; ?>">
-                <br>
+                        <label for="startJob">Mulai</label>
+                        <input class="form-control" type="datetime-local" name="startJob" id="startJob"
+                            value="<?= $dataEdit['mulai_kerja']; ?>">
+                        <br>
 
-                <label for="finishJob">Selesai</label>
-                <input class="form-control" type="datetime-local" name="finishJob" id="finishJob" value="<?= $dataEdit['selesai_kerja']; ?>">
-                <br>
+                        <label for="finishJob">Selesai</label>
+                        <input class="form-control" type="datetime-local" name="finishJob" id="finishJob"
+                            value="<?= $dataEdit['selesai_kerja']; ?>">
+                        <br>
 
-                <div class="radio">
-                    <p>Tempat Workshop</p>
-                    <?php $tw = $dataEdit['tempat_workshop']; ?>
-                    <label class="mx-2" for="whereWorkshop"><input class='mx-1' type="radio" name="whereWorkshop"
-                            value="Surabaya" <?php echo ($tw == 'Surabaya') ? "checked" : "" ?>>Surabaya</label>
-                    <label class="mx-2" for="whereWorkshop"><input class='mx-1' type="radio" name="whereWorkshop"
-                            value="Malang" <?php echo ($tw == 'Malang') ? "checked" : "" ?>>Malang</label>
-                    <label class="mx-2" for="whereWorkshop"><input class='mx-1' type="radio" name="whereWorkshop"
-                            value="Kediri" <?php echo ($tw == 'Kediri') ? "checked" : "" ?>>Kediri</label>
-                </div>
-                <br>
+                        <div class="radio">
+                            <p>Tempat Workshop</p>
+                            <?php $tw = $dataEdit['tempat_workshop']; ?>
+                            <label class="mx-2" for="whereWorkshop"><input class='mx-1' type="radio"
+                                    name="whereWorkshop" value="Surabaya"
+                                    <?php echo ($tw == 'Surabaya') ? "checked" : "" ?>>Surabaya</label>
+                            <label class="mx-2" for="whereWorkshop"><input class='mx-1' type="radio"
+                                    name="whereWorkshop" value="Malang"
+                                    <?php echo ($tw == 'Malang') ? "checked" : "" ?>>Malang</label>
+                            <label class="mx-2" for="whereWorkshop"><input class='mx-1' type="radio"
+                                    name="whereWorkshop" value="Kediri"
+                                    <?php echo ($tw == 'Kediri') ? "checked" : "" ?>>Kediri</label>
+                        </div>
+                        <br>
 
-                <div class="radio">
-                    <p>Desainer</p>
-                    <?php
-                    while ($resultDesainer = mysqli_fetch_array($queryDesainer)) {
-                        if ($dataEdit['nama_desainer'] == $resultDesainer['desainerName']) {
-                            echo "<label class='mx-2' for='isDesainer'><input type='radio' class='mx-1' name='isDesainer' value='" . $resultDesainer['desainerName'] . "' checked>" . $resultDesainer['desainerName'] . "</label>";
-                        } else {
-                            echo "<label class='mx-2' for='isDesainer'><input type='radio' class='mx-1' name='isDesainer' value='" . $resultDesainer['desainerName'] . "'>" . $resultDesainer['desainerName'] . "</label>";
-                        }
-                    }
-                    ?>
-                </div>
-                <br>
-
-                <div class="radio">
-                    <p>Operator</p>
-                    <?php while ($resultOperator = mysqli_fetch_array($queryOperator)) {
+                        <div class="radio">
+                            <p>Operator</p>
+                            <?php while ($resultOperator = mysqli_fetch_array($queryOperator)) {
                         if ($dataEdit['nama_operator'] == $resultOperator['operatorName']) {
                             echo "<label class='mx-2' for='isOperator'><input type='radio' class='mx-2' name='isOperator' value='" . $resultOperator['operatorName'] . "' checked>" . $resultOperator['operatorName'] . "</label>";
                         } else {
                             echo "<label class='mx-2' for='isOperator'><input type='radio' class='mx-2' name='isOperator' value='" . $resultOperator['operatorName'] . "'>" . $resultOperator['operatorName'] . "</label>";
                         }
                     }
-                    ?>
-                </div>
-                <br>
+?>
+                        </div>
+                        <br>
 
-                <div class="radio">
-                    <p>Finishing</p>
-                    <?php
-                    $checkedFinishing = explode(",", $dataEdit['nama_finishing']);
-                    while ($resultFinishing = mysqli_fetch_array($queryFinishing)) {
-                        if (in_array($resultFinishing['operatorName'], $checkedFinishing)) {
-                            echo "<label class='mx-2' for='isFinishing'><input type='checkbox' class='mx-2' name='isFinishing[]' value='" . $resultFinishing['operatorName'] . "' checked>" . $resultFinishing['operatorName'] . "</label>";
-                        } else {
-                            echo "<label class='mx-2' for='isFinishing'><input type='checkbox' class='mx-2' name='isFinishing[]' value='" . $resultFinishing['operatorName'] . "'>" . $resultFinishing['operatorName'] . "</label>";
-                        }
-                    }
-                    ?>
-                </div>
-                <br>
+                        <div class="radio">
+                            <p>Finishing</p>
+                            <?php
+$checkedFinishing = explode(",", $dataEdit['nama_finishing']);
+while ($resultFinishing = mysqli_fetch_array($queryFinishing)) {
+    if (in_array($resultFinishing['operatorName'], $checkedFinishing)) {
+        echo "<label class='mx-2' for='isFinishing'><input type='checkbox' class='mx-2' name='isFinishing[]' value='" . $resultFinishing['operatorName'] . "' checked>" . $resultFinishing['operatorName'] . "</label>";
+    } else {
+        echo "<label class='mx-2' for='isFinishing'><input type='checkbox' class='mx-2' name='isFinishing[]' value='" . $resultFinishing['operatorName'] . "'>" . $resultFinishing['operatorName'] . "</label>";
+    }
+}
+?>
+                        </div>
+                        <br>
 
-                <p>Quality Control</p>
-                <div class="radio">
-                    <label class="mx-2" for="isQC">
-                        <?php ?>
-                        <input class="mx-2" type="radio" name="isQC" value="Ghofar"
-                            <?php echo ($dataEdit['nama_qc']) ? "checked" : "" ?>>Ghofar
-                        <input class="mx-2" type="radio" name="isQC" value="Arvidi"
-                            <?php echo ($dataEdit['nama_qc']) ? "checked" : "" ?>>Arvidi
-                    </label>
-                </div>
-                <br>
+                        <p>Quality Control</p>
+                        <div class="radio">
+                            <label class="mx-2" for="isQC">
+                                <?php ?>
+                                <input class="mx-2" type="radio" name="isQC" value="Ghofar"
+                                    <?php echo ($dataEdit['nama_qc']) ? "checked" : "" ?>>Ghofar
+                                <input class="mx-2" type="radio" name="isQC" value="Arvidi"
+                                    <?php echo ($dataEdit['nama_qc']) ? "checked" : "" ?>>Arvidi
+                            </label>
+                        </div>
+                        <br>
 
-                <input type="hidden" name="statusAntrian" value="<?php echo $dataEdit['status_antrian'] ?>">
+                        <input type="hidden" name="statusAntrian" value="<?php echo $dataEdit['status_antrian'] ?>">
 
-                <input type="submit" value="Ubah" name="ubah" class="btn btn-warning btn-block">
-                <br><br>
+                        <input type="submit" value="Ubah" name="ubah" class="btn btn-warning btn-block">
+                        <br><br>
+                    </div>
+                </form>
             </div>
-        </form>
-    </div>
-    </div>
+        </div>
     </div>
 </body>
 
